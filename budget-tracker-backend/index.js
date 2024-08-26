ff61f7bae0cc50b3bb586e46b56e5e90dd1df9bc
ijTuhin <isratishu0209@gmail.com>
2024-08-27 03:49:49 +0600

    mongoose running on backend

---
+++ budget-tracker-backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(cors()); // using middleware
+app.use(express.json()); // to Body parse
+
+require("dotenv").config();
+
+/* *******************
+DB connection with Mongoose
+******************** */
+const database = (module.exports = () => {
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dbhost.ueaoans.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    )
+    .then(() => console.log("Successful Connection"))
+    .catch((err) => console.log(err));
+});
+
+database();
+
+app.listen(3001, () => {
+  console.log("Mongoose Server running");
+});
--- budget-tracker-frontend/src/components/DeleteItem.jsx
+++ budget-tracker-frontend/src/components/DeleteItem.jsx
@@ -4,9 +4,12 @@ export default function DeleteItem() {
   return (
     <button
       type="button"
-      className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-500 hover:text-red-600 focus:outline-none focus:text-red-800 disabled:opacity-50 disabled:pointer-events-none"
+      className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-500 hover:text-red-600"
     >
-      Remove <span className="text-xl"><MdDeleteForever /></span>
+      Remove
+      <span className="text-xl">
+        <MdDeleteForever />
+      </span>
     </button>
   );
 }
