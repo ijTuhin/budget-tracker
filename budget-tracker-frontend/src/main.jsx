5bdc64c8fd7f0faf14877eb0e119b2fc7de071dd
ijTuhin <isratishu0209@gmail.com>
2024-08-27 06:20:42 +0600

    login page added

--- budget-tracker-frontend/src/main.jsx
+++ budget-tracker-frontend/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Homepage from "./pages/Homepage.jsx";
+import LoginPage from "./authentication/LoginPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    // errorElement: <ErrorPage />,
+    // loader: () => {
+    //   return fetch(
+    //     `http://localhost:3001/payment?month=${month}&item=meal`,
+    //     {
+    //       headers: { Authorization: `Beared ${token}` },
+    //     }
+    //   );
+    // },
+  },
+  {
+    path: "/signup",
+    element: <LoginPage />,
+    // errorElement: <ErrorPage />,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
